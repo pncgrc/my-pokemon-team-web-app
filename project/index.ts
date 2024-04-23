d6edce9f8de1c5e2e5438f361637f9e20f866d14
Ponci Garcia <s080037@ap.be>
2024-04-23 16:39:00 +0000

    Zoekfunctie aan het werk gekregen

--- project/index.ts
+++ project/index.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import ejs from "ejs";
 import { Pokemon } from "./interfaces/pokemon-interfaces";
 
 const app = express();
@@ -8,17 +7,25 @@ app.set("view engine", "ejs");
 app.set("port", 3000);
 app.use(express.static("public"));
 
-async function FetchPokemonData() {
+async function fetchData() {
     const response = await fetch("https://raw.githubusercontent.com/AP-G-1PRO-Webontwikkeling/project-webontwikkeling-pncgrc/main/project/pokemon.json");
-    const pokeData: Pokemon[] = await response.json();
-
-    app.get("/", (req, res) => {
-        res.render("index", {pokeData: pokeData});    
-    });
-    
+    const pokemonData: Pokemon[] = await response.json();
+    return pokemonData;
 }
 
-FetchPokemonData();
-
+app.get("/", async (req, res) => {
+    let pokeData: Pokemon[] = await fetchData();
+    let q: string = "";
+    if (typeof req.query.q === "string") {
+        q = req.query.q;
+    }
+    pokeData = pokeData.filter((pokemon) => {
+        return pokemon.name.toLowerCase().startsWith(q.toLowerCase());
+    })
+    res.render("index", {
+        pokeData: pokeData,
+        q: q
+    });
+});
 
 app.listen(app.get("port"), ()=>console.log( "[server] http://localhost:" + app.get("port")));
